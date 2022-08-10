90a41b49cc57db73b69e5fbd682c43325fa8df85
Mohammad <moh.hossain98@gmail.com>
2022-08-10 13:38:13 -0400

    commit tree 8c706972644c814a6132a0231c03d230562905de

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+//destructive
+const destructivelyAppendCat= name => cats.push(name)
+const destructivelyPrependCat = name => cats.unshift(name)
+const destructivelyRemoveFirstCat = () => cats.shift()
+const destructivelyRemoveLastCat = () => cats.pop()
+
+
+//non-destructive
+const appendCat= name =>  [...cats, name]
+const prependCat= name => [name, ...cats]
+const removeFirstCat = () => cats.splice(1)
+const removeLastCat = () => cats.splice(0, cats.length - 1)
